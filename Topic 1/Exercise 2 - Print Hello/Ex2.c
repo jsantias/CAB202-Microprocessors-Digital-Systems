daec09da48bae6ecf9492f74d2835ef155937c40
John Santias <32500331+AsianJohnBoi@users.noreply.github.com>
2017-12-04 11:51:06 +1000

    Create Ex2.c

---
+++ Topic 1/Exercise 2 - Print Hello/Ex2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void print_hello( void ) {
+	// (a) Use printf() to say hello to your student number, on a line by itself.
+	printf("Hello n9983244!\n");
+}
+
+int main( void ) {
+	print_hello();
+	return 0;
+}
